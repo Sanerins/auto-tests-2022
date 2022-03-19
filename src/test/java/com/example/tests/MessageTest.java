2715a8f0971a17fdfe53ea55689dbf4fd7cf74dc
Sanerins <askosnitskiy@gmail.com>
2022-03-19 23:44:41 +0300

    Implement first test

--- src/test/java/com/example/tests/MessageTest.java
+++ src/test/java/com/example/tests/MessageTest.java
@@ -1,17 +1,32 @@
 package com.example.tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-public class MessageTest implements Drivable {
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class MessageTest {
 
     @Test
-    private static void openMessages() {
-        login();
-
+    public void openMessages() {
+        logIn("piven", "secret");
+        $(By.xpath("//div[@id='msg_toolbar_button']")).click();
+        $(By.xpath("//input[contains(@class, 'base-input')]")).shouldBe(appear);
+        logOff();
     }
 
-    private static void login() {
-        
+    public static void logIn(String login, String pass) {
+        open("https://ok.ru");
+        $(By.id("field_email")).setValue(login);
+        $(By.id("field_password")).setValue(pass);
+        $(By.xpath("//input[@data-l='t,sign_in']")).click();
+    }
+
+    public static void logOff() {
+        $(By.xpath("//div[contains(@class, 'ucard-mini toolbar_ucard js-toolbar-menu')]")).click();
+        $(By.xpath("//a[@data-l='t,logout']")).click();
+        $(By.id("hook_FormButton_logoff.confirm_not_decorate")).click();
     }
 
 }
