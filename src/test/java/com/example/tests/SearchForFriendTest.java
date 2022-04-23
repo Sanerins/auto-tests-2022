60606e1261326b3619ecb9c3b0acb9db4bac9d2e
Sanerins <askosnitskiy@gmail.com>
2022-04-23 23:35:32 +0300

    Delete unused exception

--- src/test/java/com/example/tests/SearchForFriendTest.java
+++ src/test/java/com/example/tests/SearchForFriendTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +16,7 @@ public class SearchForFriendTest extends BaseTest {
     @Tag("Friends")
     @DisplayName("Find non existent friends test")
     @MethodSource("provideNonExistentFriends")
-    public void findNonExistentFriendTest(String nonExistentFriendName) throws IOException {
+    public void findNonExistentFriendTest(String nonExistentFriendName) {
         FeedPage.openPage();
         assertThat(new FeedPage()
                 .openFriends()
